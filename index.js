91fcf15a2715907d32f1315611e4ed39f45ba3f9
Burhanuddin Hadianto <burhanuddin@mggsoftware.com>
2020-01-21 10:10:11 +0700

    update output

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const newman = require('newman')
 const PARALLEL_RUN_COUNT = 2
 
 const parametersForTestRun = {
-    collection: path.join(__dirname, 'postman/postman_collection.json'), // your collection
-    environment: path.join(__dirname, 'postman/localhost.postman_environment.json'), //your env
+    collection: path.join(__dirname, 'postman/Sequence Funding Accept - Test.postman_collection.json'), // your collection
+    environment: path.join(__dirname, 'postman/Wallex Burhan - Local.postman_environment.json'), //your env
     reporters: 'cli'
 };
 
@@ -25,7 +25,26 @@ async.parallel(
     (err, results) => {
         err && console.error(err);
 
+        // console.log('>>>>> result: ', results);
+
         results.forEach(function (result) {
+
+            // console.log('>>>> result e: ', result.run.executions);
+
+
+            result.run.executions.forEach(execution => {
+
+                // console.log('>>>> execution: ', execution);
+                // console.log('>>> response: ', execution.response.stream.toString());
+                console.log('>>> request: ');
+                // console.log(execution.request);
+                console.log(`${execution.request.url.protocol}://${execution.request.url.host.join('.')}:${execution.request.url.port}/${execution.request.url.path.join('/')}`);
+                console.log('>>> response: ');
+                console.log(execution.response.stream.toString());
+
+            });
+
+
             var failures = result.run.failures;
             console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
                 `${result.collection.name} ran successfully.`);
