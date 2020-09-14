3d314c154729e0a6118c4df1a6ed6320551c60ad
Burhanuddin Hadianto <burhanuddin.hadianto@gmail.com>
2020-09-14 10:17:28 +0700

    test concurrent request content

--- index.js
+++ index.js
@@ -3,27 +3,57 @@ const async = require('async')
 const newman = require('newman')
 const beautify = require('json-beautify')
 
-const PARALLEL_RUN_COUNT = 2
+const PARALLEL_RUN_COUNT = 10
 
 const parametersForTestRun = {
-    // collection: path.join(__dirname, 'postman/Sequence-Funding-Accept-1-Test-case.postman_collection.json'), // your collection
-    // collection: path.join(__dirname, 'postman/Sequence-Funding-Accept-2-Test-case.postman_collection.json'), // your collection
-    // collection: path.join(__dirname, 'postman/Sequence-Funding-Accept-Test-No-Accept .postman_collection.json'), // your collection
-    collection: path.join(__dirname, 'postman/Sequence-Funding-Accept-Test.postman_collection.json'), // your collection
-    environment: path.join(__dirname, 'postman/Wallex-Burhan-Local.postman_environment.json'), //your env
+    collection: path.join(__dirname, 'postman/Test-Concurrent-Request-Context.postman_collection_user.json'), // your collection
+    environment: path.join(__dirname, 'postman/B2B2X-Local.postman_environment.json'), //your env
     // reporters: 'json', // cli, json, junit, progress and emojitrain
     reporters: 'cli', // cli, json, junit, progress and emojitrain
     color: 'off' // on, off and auto
 };
 
-parallelCollectionRun = (done) => {
+const parametersForTestRun2 = {
+    collection: path.join(__dirname, 'postman/Test-Concurrent-Request-Context.postman_collection_admin.json'), // your collection
+    environment: path.join(__dirname, 'postman/B2B2X-Local.postman_environment.json'), //your env
+    // reporters: 'json', // cli, json, junit, progress and emojitrain
+    reporters: 'cli', // cli, json, junit, progress and emojitrain
+    color: 'off' // on, off and auto
+};
+
+const parametersForTestRun3 = {
+    collection: path.join(__dirname, 'postman/Test-Concurrent-Request-Context.postman_collection_user2.json'), // your collection
+    environment: path.join(__dirname, 'postman/B2B2X-Local.postman_environment.json'), //your env
+    // reporters: 'json', // cli, json, junit, progress and emojitrain
+    reporters: 'cli', // cli, json, junit, progress and emojitrain
+    color: 'off' // on, off and auto
+};
+
+
+let parallelCollectionRun = (done) => {
     newman.run(parametersForTestRun, done);
 };
 
+let parallelCollectionRun2 = (done) => {
+    newman.run(parametersForTestRun2, done);
+};
+
+let parallelCollectionRun3 = (done) => {
+    newman.run(parametersForTestRun3, done);
+};
+
+
+// let commands = [ 
+//     parallelCollectionRun, 
+//     parallelCollectionRun2 
+// ];
 let commands = []
 for (let index = 0; index < PARALLEL_RUN_COUNT; index++) {
     commands.push(parallelCollectionRun);
+    commands.push(parallelCollectionRun2);
+    commands.push(parallelCollectionRun3);
 }
+
 
 // Runs the Postman sample collection thrice, in parallel.
 async.parallel(
